1dfe773db3370a28c822702154703efb67583003
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-17 05:01:04 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,5 +2,12 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
+  include dance_module
+  
+  attr_accessor :name 
+  
+  def intialize(name)
+    @name = name
+  end
   
 end
